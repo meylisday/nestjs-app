c9d560804abbfe915478d13b4213504fadde9c76
meylisday <victory.sett@gmail.com>
2023-05-05 14:36:38 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT;
+  const port = process.env.PORT || 5000;
   await app.listen(port);
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   logger.log(`Application listetin on port ${port}`);
