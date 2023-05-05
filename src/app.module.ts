aad7e74f279ea135df6782975b434e844105f40d
meylisday <victory.sett@gmail.com>
2023-05-05 15:54:30 +0200

    Update configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
 import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { CatsModule } from './cats/cats.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    // MongooseModule.forRoot(
-    //   'mongodb://127.0.0.1:27017/?readPreference=primary&ssl=false&directConnection=true',
-    //   {
-    //     dbName: 'cats',
-    //   },
-    // ),
-    // CatsModule,
+    MongooseModule.forRoot(
+      // 'mongodb://127.0.0.1:27017/?readPreference=primary&ssl=false&directConnection=true',
+      'mongodb+srv://admin:l638OD3vmJN5yDjT@nest.7ffphcn.mongodb.net/?retryWrites=true&w=majority',
+      {
+        dbName: 'cats',
+      },
+    ),
+    CatsModule,
     UsersModule,
     AuthModule,
   ],
