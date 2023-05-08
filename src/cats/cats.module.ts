3db6970660940ecf9529365e67e9e4828a698766
meylisday <victory.sett@gmail.com>
2023-05-08 11:30:51 +0200

    Update config

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -5,9 +5,20 @@ import { CatsController } from './cats.controller';
 import { Cat, CatSchema } from './schemas/cat.schema';
 import { IsUnique } from './dto/validators/is-unique.validator';
 import { AuthModule } from '../auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
     MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }]),
     AuthModule,
   ],
